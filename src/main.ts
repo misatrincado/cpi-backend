a28ad81b8e074e8ce3b938ca0e2050b4a0d41960
Misael <misael.trincado@algramo.com>
2022-04-29 18:14:05 -0400

    add ssl crt

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import * as fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync('./src/secrets/private.key'),
+  cert: fs.readFileSync('./src/secrets/certificate.crt'),
+  ca: [
+    fs.readFileSync('./src/secrets/ca_bundle.crt')
+  ]
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions
+  });
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.use(bodyParser.json({ limit: '50mb' }));
   app.enableCors();
