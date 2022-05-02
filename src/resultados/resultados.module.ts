2780f8e51878537b0bfe4dacb7d659343d266df2
Misael <misael.trincado@algramo.com>
2022-05-02 18:46:17 -0400

    indicador

--- src/resultados/resultados.module.ts
+++ src/resultados/resultados.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ambito } from 'src/ambito/ambito.entity';
 import { Calificacion } from 'src/calificacion/calificacion.entity';
+import { Indicador } from 'src/indicador/indicador.entity';
 import { Parametro } from 'src/parametro/parametro.entity';
 import { Subambito } from 'src/subambito/subambito.entity';
 import { ResultadosController } from './resultados.controller';
@@ -15,7 +16,8 @@ import { ResultadosService } from './resultados.service';
       Subambito,
       Ambito,
       Parametro,
-      Calificacion
+      Calificacion,
+      Indicador
     ]),
   ],
   controllers: [ResultadosController],
